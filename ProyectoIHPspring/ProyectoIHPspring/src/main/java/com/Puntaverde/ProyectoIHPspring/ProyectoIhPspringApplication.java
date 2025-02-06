ac7a4e387ffa6a7b26002cb34178e6b66a762a02
danielghierro <dgabhie3107@g.educaand.es>
2025-02-06 09:41:10 +0100

    subo el proyecto de tipo spring

---
+++ ProyectoIHPspring/ProyectoIHPspring/src/main/java/com/Puntaverde/ProyectoIHPspring/ProyectoIhPspringApplication.java
@@ -0,0 +1,13 @@
+package com.Puntaverde.ProyectoIHPspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIhPspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIhPspringApplication.class, args);
+	}
+
+}
---
+++ ProyectoIHPspring/ProyectoIHPspring/src/test/java/com/Puntaverde/ProyectoIHPspring/ProyectoIhPspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Puntaverde.ProyectoIHPspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIhPspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
